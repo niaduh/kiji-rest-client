03737db60cf567255e4f8b12dce35246cc2cd1df
amit <amit@wibidata.com>
2013-05-15 14:22:32 -0700

    Add some comments.

--- lib/kijirest/client.rb
+++ lib/kijirest/client.rb
@@ -21,6 +21,10 @@ require 'json'
 require 'cgi'
 
 module KijiRest
+
+  #
+  # Provides a simple wrapper interface around the KijiREST server. The return of most
+  # of the methods are parsed JSON.
   class Client
 
     #Error class that wraps exceptions thrown by the server
@@ -31,33 +35,57 @@ module KijiRest
       end
     end
 
-    #Constructor for the KijiRest::Client. The only optional argument is the base_uri
-    #defining the location of the server.
+    #
+    # Constructor for the KijiRest::Client. The only optional argument is the base_uri
+    # defining the location of the server.
     def initialize(base_uri="http://localhost:8080")
       @base_uri=base_uri
       @version="v1"
     end
 
+    #
+    #Returns a list of visible instances
     def instances
       get_json(instances_endpoint)
     end
 
+    #
+    # Returns the instance description
+    # param: instance_name is the name of the instance to describe.
     def instance(instance_name)
       get_json(instance_endpoint(instance_name))
     end
 
+    # Returns a single row from the given table.
+    # param: instance_name is the name of the instance
+    # param: table_name is the name of the table
+    # param: rowkey is the rowkey to fetch
+    # param: filters is a hash containing any filters to apply which will get translated as
+    #        query parameters in the REST call.
     def row(instance_name, table_name, rowkey,filters={})
       get_json("#{rows_endpoint(instance_name, table_name)}/#{rowkey}",filters)
     end
 
+    # Returns a list of the tables in the given instance.
+    # param: instance_name is the name of the instance
     def tables(instance_name)
       get_json(tables_endpoint(instance_name))
     end
 
+    # Returns the table layout for a given table.
+    # param: instance_name is the name of the instance
+    # param: table_name is the name of the table
     def table(instance_name,table_name)
       get_json(table_endpoint(instance_name, table_name))
     end
 
+    # Performs a scan (via HTTP streaming) yielding each row to the given block.
+    # param: instance_name is the name of the instance
+    # param: table_name is the name of the table
+    # param: filters is a hash containing any filters to apply which will get translated as
+    #        query parameters in the REST call.
+    # param: block is an anonymous block of code that will be given a hash representing the
+    #        current row.
     def rows(instance_name,table_name,filters={},&block)
       if !block
         raise "No block given!"
@@ -99,6 +127,7 @@ module KijiRest
 
     private
 
+    #BEGIN endpoint definitions
     def instances_endpoint
       "#{@base_uri}/#{@version}/instances"
     end
@@ -119,6 +148,14 @@ module KijiRest
       "#{table_endpoint(instance_name, table_name)}/rows"
     end
 
+    #END endpoint definitions
+
+    # Generic method that will raise a KijiRestClientError that contains a JSON object
+    # with the error in question. In case the exception from the server is not JSON encoded,
+    # return a generic JSON message with the message sent by the server so that the client
+    # has a consistent interface.
+    #
+    # param: response_body is the contents of the response.
     def raise_exception(response_body)
       begin
         raise KijiRestClientError.new(JSON.parse(response_body))
@@ -129,6 +166,9 @@ module KijiRest
       end
     end
 
+    # Returns a parsed JSON object for the given endpoint and query parameters
+    # param: endpoint is the HTTP endpoint
+    # param: query_params are query parameters to pass along to the endpoint
     def get_json(endpoint,query_params={})
       url_query_params = query_params.map {|k,v| "#{k}=#{CGI.escape(v.to_s)}"}.join("&")
 
--- lib/kijirest/server.rb
+++ lib/kijirest/server.rb
@@ -22,10 +22,10 @@ require 'open-uri'
 
 module KijiRest
 
-  #Class that wraps the starting/stopping of a KijiREST server.
-  #assumes that this is running as a non-privileged user that
-  #has permissions to read/write to the location where KIJI_REST is
-  #installed
+  # Class that wraps the starting/stopping of a KijiREST server.
+  # assumes that this is running as a non-privileged user that
+  # has permissions to read/write to the location where KIJI_REST is
+  # installed
   class Server
     KIJI_REST_HOME="/opt/wibi/kiji-rest"
     def initialize(kiji_rest_home=KIJI_REST_HOME)
@@ -37,8 +37,13 @@ module KijiRest
       reload_configuration!
     end
 
-    #Start a server given a zookeeper quorum and array of visible
-    #instances. This will modify the configuration.yml and launch the server.
+    # Start a server given a zookeeper quorum and array of visible
+    # instances. This will modify the configuration.yml and launch the server.
+    # param: zk_quorum is the list of zookeeper servers. Default is .env
+    # param: visible_instances is an array of instance names that are to be visible by clients
+    #        of the REST server.
+    # param: wait_for_load specifies whether or not to block until the server has come up. If the
+    #        server fails to come up after some period of time, an exception will be raised.
     def start(zk_quorum=".env",visible_instances=["default"], wait_for_load=false)
       if running?
         raise "KijiREST appears to be running."
@@ -67,7 +72,9 @@ module KijiRest
       end
     end
 
-    #Stops the server optionally waiting for the server to shutdown
+    # Stops the server optionally waiting for the server to shutdown
+    # param: wait_for_shutdown determines whether or not to wait for the server to have shutdown
+    #        before returning.
     def stop(wait_for_shutdown=false)
       pid_file = qualify_file("kiji-rest.pid")
       if File.exist?(pid_file)
@@ -91,15 +98,15 @@ module KijiRest
       end
     end
 
-    #Returns a new client instance.
+    # Returns a new client instance.
     def new_client
       Client.new("http://localhost:#{@http_port}")
     end
 
     private
 
-    #Reloads the configuration.yml storing the configuration in a private member
-    #variable.
+    # Reloads the configuration.yml storing the configuration in a private member
+    # variable.
     def reload_configuration!
       @dropwizard_config = YAML.load_file(qualify_file("/conf/configuration.yml"))
       @http_port = 8080
@@ -108,13 +115,13 @@ module KijiRest
       end
     end
 
-    #Checks if this current user is authorized to run the server. A simple check
-    #is to ensure that the current user is the owner of a known file (conf/configuration.yml)
+    # Checks if this current user is authorized to run the server. A simple check
+    # is to ensure that the current user is the owner of a known file (conf/configuration.yml)
     def authorized_to_run?
       File.owned?(qualify_file("/conf/configuration.yml"))
     end
 
-    #Checks if the application is running by hitting a known URI
+    # Checks if the application is running by hitting a known URI
     def app_running?
       begin
         f = open("http://localhost:#{@http_port}/v1/instances")
@@ -125,17 +132,17 @@ module KijiRest
       end
     end
 
-    #Checks if the pid file exists. This will check for the pid file generated
-    #when the server is launched by a non-privileged user as well as the pid file
-    #generated when this server is launched by /sbin/service.
+    # Checks if the pid file exists. This will check for the pid file generated
+    # when the server is launched by a non-privileged user as well as the pid file
+    # generated when this server is launched by /sbin/service.
     def pid_exists?
       local_pid_file=qualify_file("kiji-rest.pid")
       global_pid_file="/var/run/kiji-rest/kiji-rest.pid"
       File.exists?(local_pid_file) || File.exists?(global_pid_file)
     end
 
-    #Checks if the server is running by checking the process file (kiji-rest.pid)
-    #or the global/root run /var/run/kiji-rest.pid
+    # Checks if the server is running by checking the process file (kiji-rest.pid)
+    # or the global/root run /var/run/kiji-rest.pid
     def running?
       app_running? || pid_exists?
     end
